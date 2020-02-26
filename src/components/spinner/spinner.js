78a5d752033b2dd3d7d1458cea319b23dbcccbe3
Владислав Бородин <vb@rocketstation.ru>
2020-02-26 17:51:27 +0300

    add preload and error logic

---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import './spinner.css';
+
+const Spinner = () => {
+  return(
+    <div className="loadingio-spinner-eclipse-gtbtv3q6hue">
+      <div className="ldio-7rzfrclzf8m">
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default  Spinner;
